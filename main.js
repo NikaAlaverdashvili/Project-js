b9e4fb8cc0b9bedcd10d8068f482d1c90cea7026
NikaAlaverdashvili <nikaalaverdashvili8@gmail.com>
2024-04-06 21:47:51 +0400

    add files

---
+++ main.js
@@ -0,0 +1,18 @@
+//navigation
+
+const button = document.querySelector('.navigation-button');
+
+button.addEventListener('click', function() {
+  window.location.href = 'navigation.html'; 
+});
+
+const webName = document.querySelector('.Web-Name');
+
+webName.addEventListener('click', function() {
+  window.location.href = 'index.html'; 
+});
+
+
+
+
+
