2e871ae88d578f87b1f1929d19492775b5d3b7b7
NikaAlaverdashvili <nikaalaverdashvili8@gmail.com>
2024-04-07 13:04:07 +0400

    add advertisiment item

--- main.js
+++ main.js
@@ -30,7 +30,70 @@ function shrinkAndRestore(selector) {
 
 
 
+  
+
+//autoChangeImage
+
+  const imageFiles = ['image1.jpg', 'image2.jpg', 'image3.jpg', 'image4.jpg'];
+  
+  let currentIndex = 0;
+  
+
+  function loadImages() {
+    const container = document.getElementById('imageContainer');
+    container.innerHTML = '';
+    
+   
+    const img = document.createElement('img');
+    img.src = 'img/' + imageFiles[currentIndex];
+    
+
+    img.addEventListener('click', function() {
+      nextImage();
+    });
+    
+
+    container.appendChild(img);
+  }
+  
+
+  function nextImage() {
+    currentIndex = (currentIndex + 1) % imageFiles.length;
+    loadImages();
+  }
+
+  function autoChangeImage() {
+    setInterval(function() {
+      nextImage();
+    }, 5000);
+  }
+  
+  loadImages();
+  autoChangeImage();
+
+
+
+//bottom-border
+  const divItemWidth = document.querySelector('.div-item').offsetWidth;
+  const bottomBorderWidth = divItemWidth * 0.8;
+  document.querySelector('.bottom-border').style.width = bottomBorderWidth + 'px';
 
 
 
 
+  const divItems = document.querySelectorAll('.div-item');
+
+  divItems.forEach(function(divItem) {
+      const bottomBorder = divItem.querySelector('.bottom-border');
+
+      function removeBottomBorder() {
+          bottomBorder.style.display = 'none';
+      }
+
+      function showBottomBorder() {
+          bottomBorder.style.display = 'block';
+      }
+
+      divItem.addEventListener('mouseenter', removeBottomBorder);
+      divItem.addEventListener('mouseleave', showBottomBorder);
+  });
